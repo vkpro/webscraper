d3b2a7295499106d0df8cd90a2dcc3282670a7ef
Vladimir Kharitoshkin <haritoshkin@mail.ru>
2017-11-14 14:47:47 +0300

    Add logger

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,50 @@
+import logging
+import os
+import logging.config
+
 import pytest
+import json
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
-from selenium.webdriver.chrome.options import Options
-
-from selene import browser
 
 
-class MyListener(AbstractEventListener):
+def setup_logging(default_path='logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
+    """Setup logging configuration
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+class WdListener(AbstractEventListener):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def before_navigate_to(self, url, driver):
+        self.logger.info("Open {}".format(url))
+
     def before_find(self, by, value, driver):
-        print(by, value)
+        self.logger.info("Finding '{}' by '{}'".format(value, by))
 
     def after_find(self, by, value, driver):
-        print(by, value, "found")
+        self.logger.info("Found '{}' by '{}'".format(value, by))
+
+    def before_click(self, element, driver):
+        self.logger.info('Clicking  by element')
+
+    def after_click(self, element, driver):
+        self.logger.info('Element clicked')
 
     def on_exception(self, exception, driver):
-        print(exception)
+        self.logger.error(exception)
 
 
 def pytest_addoption(parser):
@@ -30,20 +60,28 @@ def browser_type(request):
 
 @pytest.fixture(scope="module")
 def remote_wd(request):
-    """URL of of remote wd e.g. http://127.0.0.1:4444/wd/hub"""
+    """URL of remote wd e.g. http://127.0.0.1:4444/wd/hub"""
     return request.config.getoption("--remote_wd")
 
 
 @pytest.fixture(scope="module")
 def wd(request, browser_type, remote_wd):
     """Webdriver object"""
+    setup_logging()
+    logger = logging.getLogger(__name__)
+
     if browser_type == "firefox":
-        wd = webdriver.Firefox()
+        # wd = webdriver.Firefox()
+        wd = EventFiringWebDriver(webdriver.Firefox(), WdListener())
+        logger.info("Browser {} started".format(browser_type))
     elif browser_type == "chrome":
         # wd = webdriver.Chrome()
-        wd = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+        wd = EventFiringWebDriver(webdriver.Chrome(), WdListener())
+        logger.info("Browser {} started".format(browser_type))
     elif browser_type == "phantomjs":
-        wd = webdriver.PhantomJS()
+        # wd = webdriver.PhantomJS()
+        wd = EventFiringWebDriver(webdriver.PhantomJS(), WdListener())
+        logger.info("Browser {} started".format(browser_type))
     elif browser_type == "rfirefox":
         wd = webdriver.Remote(remote_wd, desired_capabilities={"browserName": "firefox"})
     elif browser_type == "rphantomjs":
@@ -53,7 +91,7 @@ def wd(request, browser_type, remote_wd):
     else:
         raise ValueError("Unrecognized browser %s" % browser_type)
 
-    if browser_type != "rchrome":
+    if browser_type != 'rchrome' and 'chrome':
         wd.maximize_window()
 
     def fin():
